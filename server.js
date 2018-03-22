b96bc3f9b4087022a1c356c753009073d8f0961c
thomas <thomascac95@hotmail.fr>
2018-03-22 16:47:20 +0100

    test node server to heroku

--- server.js
+++ server.js
@@ -15,11 +15,13 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const port = process.env.PORT || '3001';
+var port = process.env.PORT || 4300;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log(`running`));
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
 
 /*app.use(bodyParser());
