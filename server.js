6417c73c0e76ad6fe98ce0709807d3d642531af9
thomas <thomascac95@hotmail.fr>
2018-03-22 16:51:38 +0100

    fix node server

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 var port = process.env.PORT || 4300;
