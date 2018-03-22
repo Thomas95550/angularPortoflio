4333ef3bcfa130b094148df6d5908dec161ecf4f
thomas <thomascac95@hotmail.fr>
2018-03-22 17:41:33 +0100

    fix node server

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
+// Get dependencies
+const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const sgMail = require('@sendgrid/mail');
+
 const api = require('./server/routes/api');
 
+var app = express();
+app.use(bodyParser());
+app.use(bodyParser.json({limit:'5mb'}));
+app.use(bodyParser.urlencoded({extended:true}));
 
-const express = require('express');
-const app = express();
-const path = require('path');
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+// API location
+app.use('/api', api);
 
-app.use(express.static(__dirname+'/dist'));
-
-app.listen(process.env.PORT || 8080);
-
-//Path Location Strategy
+app.use( express.static(__dirname + '/dist' ) );
+// Send all other requests to the Angular app
 app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'appclient', 'index.html');
-    res.sendFile(index);
+    res.sendFile(path.join(__dirname, 'appclient/index.html'));
 });
 
-console.log('Console Listening');
+//Set Port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
