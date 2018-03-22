ba6aa34016d83c5497bb3de708ebb827286af630
thomas <thomascac95@hotmail.fr>
2018-03-22 17:33:09 +0100

    fix node server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-// Get dependencies
 const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
 const api = require('./server/routes/api');
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-
-// Set our api routes
-app.use('/api', api);
+app.use(express.static(__dirname+'/dist'));
 
 app.listen(process.env.PORT || 8080);
 
