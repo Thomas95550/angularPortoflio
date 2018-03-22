3d16ad125ff4b28b2d47b5d67c442f9db04a5a0d
thomas <thomascac95@hotmail.fr>
2018-03-22 16:29:16 +0100

    pushing server node

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 // Get dependencies
-const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const http = require('http');
 const sgMail = require('@sendgrid/mail');
-
 const api = require('./server/routes/api');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
 
-var app = express();
-app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/api', api);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log(`running`));
+
+
+/*app.use(bodyParser());
 app.use(bodyParser.json({limit:'5mb'}));
 app.use(bodyParser.urlencoded({extended:true}));
-
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
-// API location
-app.use('/api', api);
-
-app.use( express.static(__dirname + '/dist' ) );
-// Send all other requests to the Angular app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-//Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-
-const server = http.createServer(app);
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+});*/
